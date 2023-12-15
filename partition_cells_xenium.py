e25168dd803a72edb4375a7b078f6320849a8574
EvanDMee <106629826+EvanDMee@users.noreply.github.com>
2023-12-15 12:36:52 -0700

    Add annotation partitioning script
    Added script used to partition cells into annotated regions.

---
+++ partition_cells_xenium.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 19 14:52:07 2023
+
+@author: emee
+"""
+
+#This script is intended to be used with standard 10x Xenium output.
+
+#This script partitions cells based on whether their centroid falls within
+#an annotated region. It appends a column for each region to the metadata csv 
+#containing a bool for each cell. If true, the cell centroid falls within the 
+#annotated region. If false, the cell centroid does not fall within the 
+#annotated region.
+
+#This script assumes that:
+#Annotations are in geojson format.
+#Each geojson file corresponds to one annotation.
+#All geojson files are in the same directory along with cells.csv.gz.
+
+import pandas as pd
+import geopandas as gpd
+import shapely
+import os
+
+#Path to directory containing geojson files (one file per annotated region)
+path = ""
+
+#Load cell metadata csv (assumed to be in same directory as geojsons)
+cells = pd.read_csv(path + "/cells.csv.gz")
+
+#Create list of geojson files found in directory
+files = []
+for file in os.listdir(path):
+    if file.endswith(".geojson"):
+        files.append(file)
+        
+#Loops through each geojson in list
+for file in files:
+    #Load and scale geojson (Xenium is always .2125 microns/pixel)
+    geojson = gpd.read_file(path + "/" + file)
+    scaled_geojson = geojson.scale(origin=(0,0), xfact = .2125, yfact = .2125)
+    
+    #Create shapely points of cell centroids
+    points = shapely.points(cells["x_centroid"], cells["y_centroid"])
+    
+    #Check if point is within geometry
+    point_list = []
+    for point in points:
+        point_list.append(shapely.contains(scaled_geojson, point))
+    
+    #Cast to bool and append column to cell metadata 
+    #Column name is geojson file name.   
+    point_list_float = list(map(float, point_list))
+    point_list_bool = list(map(bool, point_list_float))
+    cells[file] = point_list_bool
+
+#Write new metadata csv to same directory as geojsons
+cells.to_csv(path + "/partitioned_cells.csv")
